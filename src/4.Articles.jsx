584cdbb74e0bca0be44faaa65121584ae857988c
ABe-a-tron <alexbtn92@gmail.com>
2023-09-05 14:03:05 +0100

    adds completed task 5 view individual article

--- src/4.Articles.jsx
+++ src/4.Articles.jsx
@@ -1,19 +1,21 @@
-import axios from "axios";
-import { useEffect } from "react";
+import {getArticles} from './apis'
+import { useState, useEffect } from "react";
 import ArticlePreCard from "./4a.ArticlePreCard";
 
-const Articles = ({ articles, setArticles }) => {
-  useEffect(() => {
-    axios
-      .get("https://abcodesnorthcodersncnewsapp.onrender.com/api/articles")
-      .then(({ data }) => {
-        setArticles(data.articles);
-      });
-  }, []);
+const Articles = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(()=> {
+    getArticles()
+    .then((articles)=>{
+      setArticles(articles)
+    })
+  }, [])
+
 
   return (
-    <ul class="Articles">
-      <ArticlePreCard articles={articles} />
+    <ul className="Articles">
+      <ArticlePreCard articles={articles}/>
     </ul>
   );
 };
--- src/6.Article.jsx
+++ src/6.Article.jsx
@@ -1,5 +1,43 @@
+import { getArticleByID } from "./apis";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Article = () => {
+  let articleId = useParams();
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-}
+  useEffect(() => {
+    setIsLoading(true);
+    getArticleByID(articleId)
+      .then((article) => {
+        setArticle(article);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setIsError(true)
+      });
+  }, []);
 
-export default Article
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (isError) return <h1>Error!</h1>
+
+  return (
+    <article className="Article" key={article.article_id}>
+      <img
+        className="ArticleImage"
+        src={article.article_img_url}
+        alt={`Picture related to ${article.title}`}
+      />
+      <h2>{article.title}</h2>
+      <p>by {article.author}</p>
+      <p>topic: {article.topic}</p>
+      <p>{article.body}</p>
+    </article>
+  );
+};
+
+export default Article;
