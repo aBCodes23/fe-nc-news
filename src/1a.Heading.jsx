1c1fab1eaed26e6dfc9bc4440e03bf7e79354291
ABe-a-tron <alexbtn92@gmail.com>
2023-09-08 14:07:06 +0100

    adds link to heading and changes default user context to 'Not logged in'

--- src/1a.Heading.jsx
+++ src/1a.Heading.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Heading = () => {
   return (
     <div className="Heading">
+      <Link to='/'>
       <h1>ASCB-News</h1>
+      </Link>
     </div>
   );
 };
--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -3,7 +3,7 @@ import {createContext, useState} from 'react'
 export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
-    const [user, setUser] = useState('tickle122')
+    const [user, setUser] = useState('Not logged in')
 
     return <UserContext.Provider value={{user, setUser}}>
         {children}
